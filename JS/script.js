e6137501ea05c0a4917b3296616753353dbed00a
Vlad0405 <52607003+Vlad0405@users.noreply.github.com>
2019-11-24 13:13:55 +0300

    Add files via upload

---
+++ JS/script.js
@@ -0,0 +1,23 @@
+$(".card_1").hide();
+$(".button_1").click(function(){
+    $(".card_1").show(500);     
+});
+$(".close_1").click(function(){
+    $(".card_1").hide(500);     
+});
+
+$(".card_2").hide();
+$(".button_2").click(function(){
+    $(".card_2").show(500);     
+});
+$(".close_2").click(function(){
+    $(".card_2").hide(500);     
+});
+
+$(".card_3").hide();
+$(".button_3").click(function(){
+    $(".card_3").show(500);     
+});
+$(".close_3").click(function(){
+    $(".card_3").hide(500);     
+});
